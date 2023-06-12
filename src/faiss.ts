950d7a6d60d48414c38d74cf7614e6242028cea5
Serge Kovbasiuk <isserge@protonmail.com>
2023-06-12 19:51:13 +0700

    add Faiss

---
+++ src/faiss.ts
@@ -0,0 +1,45 @@
+import { FaissStore } from "langchain/vectorstores/faiss";
+import { Document } from 'langchain/document';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { StuffDocumentsChain } from 'langchain/chains';
+
+import { logger } from "./logger"
+import { config } from "./config"
+
+export class FaissDB {
+  client: FaissStore | undefined = undefined;
+
+  async init(docs: Document[]) {
+    try {
+      const embeddings = new OpenAIEmbeddings({ openAIApiKey: config.OPENAI_API_KEY });
+      const client = await FaissStore.fromDocuments(docs, embeddings);
+
+      this.client = client;
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async query(chain: StuffDocumentsChain, question: string) {
+    if (!this.client) {
+      throw new Error("FaissDB not initialized");
+    }
+
+    logger.info(`Querying index ${this.client.index} with query: ${question}`);
+
+    const queryResult = await this.client.similaritySearch(question);
+
+    if (queryResult.length) {
+      const content = queryResult.map((doc) => doc.pageContent).join(' ');
+
+      const result = await chain.call({
+        input_documents: [new Document({ pageContent: content })],
+        question,
+      })
+
+      return result.text;
+    } else {
+      logger.info(`No matches found for query ${question}`);
+    }
+  }
+}
