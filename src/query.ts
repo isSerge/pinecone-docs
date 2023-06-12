7aa65035d71a5f311f25ef04a53e59e41bcc9dea
Serge Kovbasiuk <isserge@protonmail.com>
2023-06-12 20:32:29 +0700

    use gpt-3.5-turbo model

--- src/query.ts
+++ src/query.ts
@@ -8,7 +8,11 @@ import { PineconeDB } from './pinecone';
 async function main() {
   try {
     const query = process.argv[2];
-    const llm = new OpenAI({ openAIApiKey: config.OPENAI_API_KEY });
+    const llm = new OpenAI({
+      openAIApiKey: config.OPENAI_API_KEY, 
+      temperature: 0.9,
+      modelName: "gpt-3.5-turbo",
+    });
     const chain = await loadQAStuffChain(llm);
     const db = new PineconeDB();
 
